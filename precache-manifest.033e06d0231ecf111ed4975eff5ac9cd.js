f360cf97549d734d129fe4dfef5eecafcf25c853
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-07-21 22:02:52 +0800

    Updates

---
+++ precache-manifest.033e06d0231ecf111ed4975eff5ac9cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb092842ff83d379c4b585f251bd2b15",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c9b8cf0b0c960af2317d",
+    "url": "/pokedex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/pokedex/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b8cf0b0c960af2317d",
+    "url": "/pokedex/static/js/main.a1d0fa36.chunk.js"
+  },
+  {
+    "revision": "54f04ab79eed4511d340",
+    "url": "/pokedex/static/js/runtime-main.252e0115.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
