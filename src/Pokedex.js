fe62ebce4e466283ec8657b203a1d4685a81075b
iamangelu <67723309+iamangelu@users.noreply.github.com>
2020-07-21 22:04:12 +0800

    add files

---
+++ src/Pokedex.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
+import './Pokedex.css'
+
+class Pokedex extends Component{
+    render(){
+        let title;
+        if(this.props.isWinner){
+            title = <h1 className='Pokedex-winner'>Winning Hand</h1>
+        } else {
+            title = <h1 className='Pokedex-loser'>Losing Hand</h1>
+        }
+        return(
+        <div className='Pokedex'>
+            <h4>Total experience is: {this.props.exp} </h4>
+            {title}
+            <div className='Pokedex-card'>
+                {this.props.pokemon.map((p) => (
+                    <Pokecard
+                    id = {p.id} 
+                    name = {p.name}
+                    type = {p.type}
+                    exp = {p.base_experience}
+                    />
+                ))}
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Pokedex;
